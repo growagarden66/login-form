87c581fbb9ff7b38ef62209fda4e4036f904e75c
growagarden66 <teo92943@gmail.com>
2025-11-03 21:26:51 -0300

    Delete ghost.js

--- ghost.js
+++
@@ -1,8 +0,0 @@
-const ghost = document.createElement('div');
-ghost.classList.add('ghost');
-document.body.appendChild(ghost);
-
-window.addEventListener('mousemove', (e) => {
-  ghost.style.left = `${e.clientX}px`;
-  ghost.style.top = `${e.clientY}px`;
-});
